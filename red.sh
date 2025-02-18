25082bf8c4c812f8413c6ea65f4ff35e37c61bc1
smoreno4 <s.moreno@nayarsystems.com>
2025-02-18 16:27:11 +0100

    Update red.sh

--- red.sh
+++ red.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+# Definir colores ANSI
+CYAN='\033[1;36m'
+NC='\033[0m'
+
+echo -e "${CYAN}>> Interfaces de Red Filtradas (eth, docker, br, n4m)${NC}"
+echo ""
+
+ip a | awk '
+    /: eth|: docker|: br|: n4m/ {iface=$2; gsub(":", "", iface)}
+    /inet / && $2 !~ /^127\.0\.0\.1/ {print iface "\n" $2 "\n"}
+'
