d6b4a8b5785df30cd1c45085b44c33547a772a2b
smoreno4 <s.moreno@nayarsystems.com>
2025-02-18 16:27:51 +0100

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+# Definir colores ANSI
+CYAN='\033[1;36m'
+NC='\033[0m'
+
+echo -e "${CYAN}>> Contenedores Docker en ejecución${NC}"
+docker ps --format "table {{.ID}}\t{{.Names}}\t{{.Status}}"
+echo ""
+
+echo -e "${CYAN}>> Imágenes de Docker${NC}"
+docker images --format "table {{.Repository}}\t{{.Tag}}\t{{.Size}}"
+echo ""
